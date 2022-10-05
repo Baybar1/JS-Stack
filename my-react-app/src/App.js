fdd6d05e226aeb8a867ac399a272d9c7b1e0cd15
Branley Aybar <branleyjraybar@gmail.com>
2022-10-05 19:41:29 -0400

    Form

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
 import React from 'react';
-import { State } from './components/State';
+import { Form } from './components/Form';
 
 
 
 function App() {
   return (
-    <div className="App">
-      <State lastName = {"Doe"} firstName = {"Jane"} age = {45} hairColor = {"Black"}/>
-      <State lastName = {"Smith"} firstName = {"John"} age = {88} hairColor = {"Brown"}/>
+    <div>
+      <Form />
     </div>
   );
 }
