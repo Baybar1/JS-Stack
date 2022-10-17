729da2f75164f0cda80af4f4f9aab790392a7f56
Branley Aybar <branleyjraybar@gmail.com>
2022-10-17 10:56:19 -0400

    updated

--- my-react-app/src/components/PlanetInfo.js
+++ my-react-app/src/components/PlanetInfo.js
@@ -15,8 +15,8 @@ export const PlanetInfo = () => {
         axios.get('https://swapi.dev/api/planets/?search=' + id)
         .then((response) => {
             setInfo(response.data.results)
-        axios.get(response.data.results[0].url)
-        .then((response) => {setGetPeople(response.data.url)})    
+        // axios.get(response.data.results[0].url)
+        // .then((response) => {setGetPeople(response.data.url)})    
         // console.log(getPeople)
         })
     },[])
@@ -30,30 +30,47 @@ export const PlanetInfo = () => {
         
         
     },[])
+
     
+    
+
+
+    let residentList = info.length && info.map((info,index) => {
+        return (
+            <div className={styles.keys} key = {index}>
+                
+                
+                {
+                    Object.keys(info).map((key,i) =>  {
+                        
+                            if (key === 'residents' || key === 'films' ) {
+                                return    <span  key = {key + i}>{key} : <ul> {info[key].map((item,num) => <li key = {key+num}><a href ={item} target = 'blank'>{item}</a></li>)}</ul> </span>
+                            } else if (key === 'url') {
+                                return <p  className={styles.lines} key = {key+i}> {key}: <ul><li><a href ={info[key]}>{info[key]}</a></li></ul> </p>
+                            } else {
+                                return <p  className={styles.lines} key = {key+i}> {key}: {info[key]} </p>
+                            }
+                            
+                        
+                        
+                        // key === 'residents' || key === 'films' ? <ul>{key} <li>{info[key]}</li></ul> :  <p  className={styles.lines} key = {index}> {key}: {info[key]} </p>
+                        })
+                }
+                {/* <a className={styles.click} href = {getPeople} target = 'blank'>Residents</a> */}
+                
+            </div>
+        )
+    })
+
+
+
     
     return (
         <div className= {styles.container}>
-            {
-                info.length && info.map((info,index) => {
-                    return (
-                        <div className={styles.keys} key = {index}>
-                            
-                            
-                            {
-                                Object.keys(info).filter(key => key !== 'residents' && key !== 'films').map((key,index) => ( 
-                                    
-                                    <p  className={styles.lines} key = {index}>{key}: {info[key]} </p>
-                                    
-                                ))
-                            }
-                            <a className={styles.click} href = {getPeople} target = 'blank'>Residents</a>
-                            
-                        </div>
-                    )
-                })
+            
+                {residentList}
                 
-            }
+            
             
             
         </div>
